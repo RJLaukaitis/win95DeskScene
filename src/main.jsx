265de83d3c25035d8bb676cb1a2dafc0bad9fd87
Ronald Laukaitis <rol226@lehigh.edu>
2024-05-26 16:36:03 -0700

    camera positioned

--- src/main.jsx
+++ src/main.jsx
@@ -12,8 +12,7 @@ root.render(
       fov: 39,
       near: 0.1,
       far: 2000,
-      position: [3, 7, -6],
-      // lookAt: [0,0,0],
+      position: [11, 9, -15],
     }}
   >
     <CSS3DScene />
